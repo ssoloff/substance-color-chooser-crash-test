2c0ad2ed6e615a14832037c30419a2b3447313ce
Steven Soloff <steven.soloff@gmail.com>
2018-11-17 14:20:16 -0500

    Add code required to trigger JVM crash

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,18 @@
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+import java.awt.Color;
+import javax.swing.JColorChooser;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public final class App {
+    public static void main(final String[] args) throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            try {
+                // commenting out the next line will prevent the JVM crash
+                UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel");
+                JColorChooser.showDialog(null, "Choose Color", Color.BLACK);
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
